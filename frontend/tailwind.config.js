d6d5a895858b2ac339c91bcb40df996b5d0679f0
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2023-02-05 22:47:27 +0100

    feat: setup tailwind constants and styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'open-sans': '"Open Sans", sans-serif',
+      },
+      colors: {
+        transparent: '#00000000',
+        black: {
+          main: '#2D2727',
+          secondary: '#393333',
+        },
+        orange: {
+          main: '#FF845E',
+        },
+      },
+      fontSize: {
+        sm: ['12px', '15px'],
+        md: ['16px', '20px'],
+        lg: ['20px', '24px'],
+        xl: ['32px', '36px'],
+        '2xl': ['80px', '85px'],
+      },
+      screens: {
+        xs: '660px',
+        sm: '770px',
+        md: '980px',
+        lg: '1100px',
+        xl: '1380px',
+      },
+    },
   },
   plugins: [],
 };
