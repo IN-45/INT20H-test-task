a517abd1114a03db251b3004ff255e78c17078b5
maksimdssv <maksimdssv@gmail.com>
2023-02-05 18:36:07 +0200

    feat:add basic structure to frontend

---
+++ frontend/src/pages/router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from 'react-router-dom';
+import RootLayout from '../features/layout/RootLayout';
+
+/* #TODO implement all the routes here
+ * descriptions of paths as well as suggestions should go here
+ * (dishes/recipes)/:id for dishes/recipes(choose one of the names)
+ * /products to view entire list of available products or just the products that user have(choose one or implement both?)
+ * /(dishes/recipes)/available for viewing what we can make with available products
+ * OPTIONAL BELOW ***
+ * /(dishes/recipes)/add for adding new dishes/recipes
+ * /products/add to add new products
+ * ?? sort by difficulty
+ * ?? look for closest shops
+ *  */
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [],
+  },
+]);
+
+export default router;
