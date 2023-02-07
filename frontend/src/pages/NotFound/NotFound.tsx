fefba281d5df35580d7b7a12603dd664d450e8b3
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2023-02-07 01:02:03 +0100

    fix: Not Found color heading

--- frontend/src/pages/NotFound/NotFound.tsx
+++ frontend/src/pages/NotFound/NotFound.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
 
   return (
     <div className='text-center absolute top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2'>
-      <h1 className='text-2xl text-purple-main'>Помилка 404</h1>
+      <h1 className='text-2xl text-orange-main'>Помилка 404</h1>
       <h2 className='text-xl pt-3'>Схоже, що адреса введена хибно або сторінка була видалена</h2>
     </div>
   );
