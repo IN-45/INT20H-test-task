527a3d7ff0725151d10de5c22e258266ea7baf96
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2023-02-07 20:15:28 +0100

    fix: add link to recipe card & placeholder image

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import DurationTag from '../DurationTag';
 
 interface CardProps {
   id: string;
-  image_url: string;
+  image_url?: string;
   name: string;
   cooking_time_minutes?: number;
+  type: 'product' | 'recipe';
 }
 
-const Card: React.FC<CardProps> = ({ id, name, image_url, cooking_time_minutes }) => {
-  return (
-    <div className='sm:max-w-[400px] space-y-3 hover:text-orange-main relative' id={id}>
+const Card: React.FC<CardProps> = ({ id, name, image_url, cooking_time_minutes, type }) => {
+  const getCustomStyles = () => {
+    if (type === 'recipe') {
+      return 'hover:text-orange-main';
+    }
+  };
+
+  const cardContent = (
+    <div className={'sm:max-w-[400px] space-y-3 relative ' + getCustomStyles()} id={id}>
       <img
         src={image_url}
         alt={name}
         title={name}
         className='xl:h-[246px] sm:h-[200px] h-[320px] w-full object-cover max-sm:brightness-50'
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null;
+          currentTarget.src = require('../../assets/placeholder.jpeg');
+        }}
       />
       <div className='space-y-3 max-sm:absolute max-sm:bottom-[20px] max-sm:px-4 w-full'>
         {cooking_time_minutes && (
@@ -28,6 +40,10 @@ const Card: React.FC<CardProps> = ({ id, name, image_url, cooking_time_minutes }
         <h4 className='max-sm:text-white-greyscale mt-0'>{name}</h4>
       </div>
     </div>
+  );
+
+  return (
+    <div>{type === 'recipe' ? <Link to={`/recipe/${id}`}>{cardContent}</Link> : cardContent}</div>
   );
 };
 
