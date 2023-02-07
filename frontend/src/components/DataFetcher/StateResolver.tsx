75b0f68f22a6fcddf94ecea7ccfd5ef9203daf4d
maksimdssv <maksimdssv@gmail.com>
2023-02-07 21:25:59 +0200

    fix: fix navbar styling and sign in

--- frontend/src/components/DataFetcher/StateResolver.tsx
+++ frontend/src/components/DataFetcher/StateResolver.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 import { DataState } from './DataFetcher';
+import { useNavigate } from 'react-router-dom';
 
 interface StateResolverProps {
   state: keyof typeof DataState;
@@ -9,7 +10,12 @@ interface StateResolverProps {
 }
 
 const StateResolver: FC<StateResolverProps> = ({ state, children, empty, error }) => {
+  const navigate = useNavigate();
   if (state !== 'loading' && empty) return <h1>No data here for now...</h1>;
+
+  if (error.status === 401) {
+    navigate('/sign-in')
+  }
 
   switch (state) {
     case 'error':
