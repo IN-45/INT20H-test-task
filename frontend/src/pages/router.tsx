d82eb6d8f9c4abc35c27b01665e365c1088cb55e
maksimdssv <maksimdssv@gmail.com>
2023-02-07 23:04:41 +0200

    feat: add recipes page

--- frontend/src/pages/router.tsx
+++ frontend/src/pages/router.tsx
@@ -5,18 +5,7 @@ import SignPage from './Sign/SignPage';
 
 import NotFound from './NotFound';
 import RecipePage from './RecipePage';
-
-/* #TODO implement all the routes here
- * descriptions of paths as well as suggestions should go here
- * (dishes/recipes)/:id for dishes/recipes(choose one of the names)
- * /products to view entire list of available products or just the products that user have(choose one or implement both?)
- * /(dishes/recipes)/available for viewing what we can make with available products
- * OPTIONAL BELOW ***
- * /(dishes/recipes)/add for adding new dishes/recipes
- * /products/add to add new products
- * ?? sort by difficulty
- * ?? look for closest shops
- *  */
+import Recipes from './Recipes/Recipes';
 
 const router = createBrowserRouter([
   {
@@ -30,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: 'recipe/:id',
         element: <RecipePage />,
+      },
+      {
+        path: 'recipe',
+        element: <Recipes />,
       },
       {
         path: 'products',
